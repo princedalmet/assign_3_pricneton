a4aea3496d625195b8288c56089e5b08f43ca974
Prince <99526815+princedalmet@users.noreply.github.com>
2024-12-10 12:09:23 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Initialize Flask app
+app = Flask(__name__)
+
+@app.route('/')
+def hello_ecs():
+    # Response message
+    return 'Hello from ECS Container!'
+
+# Main entry point
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Use port 5000 as defined in Dockerfile
